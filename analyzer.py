db71c8781a9b3dccc34698eba6aa1cb1775e0642
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-26 14:08:54 -0500

    add exception handler

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from fetcher import nasdaq
 import logging
+from urllib.error import HTTPError, URLError
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -18,12 +19,15 @@ for stock in stocks:
     url = f'https://finance.yahoo.com/quote/{stock}/'
 
     sheet = pd.read_html(url, flavor='bs4')[-1]
-    if 'N/A (N/A)' not in list(sheet[1]) and np.nan not in list(sheet[1]):
-        market_capital = sheet.iat[0, 1]
-        pe_ratio = sheet.iat[2, 1]
-        forward_dividend_yield = sheet.iat[5, 1]
-        analysis += f'{stock}\nCapital: {market_capital}\nPE Ratio: {pe_ratio}\nYield: {forward_dividend_yield}\n\n'
-    else:
-        logging.critical(f'Unable to get analysis for {stock}')
+    try:
+        if 'N/A (N/A)' not in list(sheet[1]) and np.nan not in list(sheet[1]):
+            market_capital = sheet.iat[0, 1]
+            pe_ratio = sheet.iat[2, 1]
+            forward_dividend_yield = sheet.iat[5, 1]
+            analysis += f'{stock}\nCapital: {market_capital}\nPE Ratio: {pe_ratio}\nYield: {forward_dividend_yield}\n\n'
+        else:
+            logging.critical(f'Unable to get analysis for {stock}')
+    except (IndexError, HTTPError, KeyError, URLError):
+        logging.error(f'Unable to analyze {stock}')
 
 print(f'Total number of stocks analyzed{n}\n\n{analysis}')
