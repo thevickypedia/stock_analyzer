8a4066791a147b333ae2fbd5a3bda1a2de8fd576
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-26 20:42:46 -0500

    implement workbook to save data as excel

--- analyzer.py
+++ analyzer.py
@@ -2,33 +2,41 @@ import pandas as pd
 import numpy as np
 from fetcher import nasdaq
 import logging
+import xlsxwriter
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
+workbook = xlsxwriter.Workbook('stocks.xlsx')
+worksheet = workbook.add_worksheet('Results')
 
 stocks = nasdaq()
 n = 0
-analysis = ''
 
 logging.info('Initializing Analysis on all NASDAQ stocks')
 
 for stock in stocks:
     n = n + 1
     url = f'https://finance.yahoo.com/quote/{stock}/'
-
-    sheet = pd.read_html(url, flavor='bs4')[-1]
     try:
+        sheet = pd.read_html(url, flavor='bs4')[-1]
         if 'N/A (N/A)' not in list(sheet[1]) and np.nan not in list(sheet[1]):
             market_capital = sheet.iat[0, 1]
             pe_ratio = sheet.iat[2, 1]
             forward_dividend_yield = sheet.iat[5, 1]
-            analysis += f'{stock}\nCapital: {market_capital}\nPE Ratio: {pe_ratio}\nYield: {forward_dividend_yield}\n\n'
+            worksheet.write(0, 0, "Stock Ticker")
+            worksheet.write(0, 1, "Capital")
+            worksheet.write(0, 2, "PE Ratio")
+            worksheet.write(0, 3, "Yield")
+            worksheet.write(n, 0, f'{stock}')
+            worksheet.write(n, 1, f'{market_capital}')
+            worksheet.write(n, 2, f'{pe_ratio}')
+            worksheet.write(n, 3, f'{forward_dividend_yield}')
         else:
             logging.critical(f'Received null values on analysis for {stock}')
     except:
         logging.error(f'Unable to analyze {stock}')
 
-print(f'Total number of stocks analyzed{n}\n\n{analysis}')
+workbook.close()
