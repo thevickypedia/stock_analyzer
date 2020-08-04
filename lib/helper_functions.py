7e7a532e1ca6218ce5425e04185532c159b3159d
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-08-03 21:17:47 -0500

    remove log level info in log files

--- lib/helper_functions.py
+++ lib/helper_functions.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup as bs
 
 log_filename = datetime.now().strftime('logs/stock_logs_%H:%M_%d-%m-%Y.log')
 logging.basicConfig(filename=log_filename, level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
+                    format='%(asctime)s %(message)s')
 logger = logging.getLogger('analyzer.py')
 
 
